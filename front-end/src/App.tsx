b575dce04c535c4afc32ffd3536da3a4d252e1fb
Asghar Ali <asgharalifs48@gmail.com>
2026-05-12 04:42:36 +0500

    setup

---
+++ front-end/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full min-h-screen bg-neutral-900 text-white p-4'>
+      Pollinkr
+    </div>
+  )
+}
+
+export default App
